fb661a72b252806b08c518e31bb2eadaea8a0292
HexaRG <67880600+HexaRG@users.noreply.github.com>
2021-02-10 21:27:41 -0500

    Fixed invalid use of syn.request

--- loader.lua
+++ loader.lua
@@ -2,16 +2,16 @@
 
 if game.PlaceId ~= 1111083356 then return end
 
-local LatestVersion = tonumber(syn.request("https://raw.githubusercontent.com/HexaRG/rb64-better-replication/master/version.txt","GET"))
+local LatestVersion = tonumber(syn.request({Url = "https://raw.githubusercontent.com/HexaRG/rb64-better-replication/master/version.txt"}).Body)
 
 if isfolder("rb64br") then
     local Version = tonumber(readfile("rb64br/version.txt"))
     if Version ~= LatestVersion then
-        writefile("rb64br/main.lua",syn.request("https://raw.githubusercontent.com/HexaRG/rb64-better-replication/master/main.lua","GET"))
+        writefile("rb64br/main.lua",syn.request({Url = "https://raw.githubusercontent.com/HexaRG/rb64-better-replication/master/main.lua"}).Body)
         writefile("rb64br/version.txt",tostring(LatestVersion))
     end
 else
     makefolder("rb64br")
     writefile("rb64br/version.txt",tostring(LatestVersion))
-    writefile("rb64br/main.lua",syn.request("https://raw.githubusercontent.com/HexaRG/rb64-better-replication/master/main.lua","GET"))
+    writefile("rb64br/main.lua",syn.request({Url = "https://raw.githubusercontent.com/HexaRG/rb64-better-replication/master/main.lua"}).Body)
 end
